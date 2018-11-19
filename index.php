742d906b2c4d9d3e9f6dace405ab088198b706b4
Aymeric Neveu <emploi.neveu@gmail.com>
2018-11-19 11:57:38 +0100

    exo7

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+  </head>
+  <body>
+    <?php
+    $int = 1;
+    while ($int <=100) {
+      echo 'On tient le bon bout' . '<br>';
+      $int+= 15;
+    }
+    ?>
+  </body>
